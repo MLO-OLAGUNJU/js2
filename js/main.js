3726994cb5fc5938ec4d43232c1a0c04aa841f18
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-14 22:16:57 +0100

    learning js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-console.log('Hello World!');
-console.log(typeof 'Manny');
+console.log("Hello World!");
+console.log(typeof "Manny");
 console.log(typeof 42);
 console.log(typeof true);
+console.log(typeof false);
